1f7e23a0bd633a1a2a29732339b5dc1afb4e7c23
Kacem Mathlouthi <kacem.math47@gmail.com>
2024-11-28 12:35:06 +0100

    One-Hot Encoding of Nominal Values

--- One-Hot Encoding of Nominal Values/OneHotEncoding.py
+++ One-Hot Encoding of Nominal Values/OneHotEncoding.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 def to_categorical(x, n_col=None):
-    n_cols = np.max(x)
-    result = []
-    for i in range(len(x)):
-        temp = [ j==x[i] for j in range(n_cols)]
-        result.append(temp)
+    n_cols = np.max(x) + 1
+    result = [[1 if j == x[i] else 0 for j in range(n_cols)] for i in range(len(x))]
     return result
