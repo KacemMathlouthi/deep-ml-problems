771b6540fc816c7b6ab9d6d57c686aafd0199a9d
Kacem Mathlouthi <kacem.math47@gmail.com>
2024-11-29 04:23:04 +0100

    Jaccard Index

---
+++ Calculate Jaccard Index for Binary Classification/JaccardIndex.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def jaccard_index(y_true, y_pred):
+    if len(y_pred) == 0 or len(y_true) == 0:
+        return 0 
+    intersect = np.sum(y_true & y_pred) 
+    union = np.sum(y_pred, axis = 0) + np.sum(y_pred, axis = 0) - intersect
+    print(intersect)
+    print(union)
+    if union == 0:
+        return 0
+    result = intersect / union
+    return round(result, 3)
